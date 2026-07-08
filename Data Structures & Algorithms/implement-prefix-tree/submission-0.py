7a77085a31bdfbac1fd4e0d21d21332190780533
Prakash Kurup <prakashkurup7@gmail.com>
2026-07-08 12:31:18 -0700

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+
+        for c in word:
+            if c not in node.children:
+                node.children[c] = Node()
+            
+            node = node.children[c]
+
+        node.end = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+
+        for c in word:
+            if c not in node.children:
+                return False
+
+            node = node.children[c]
+
+        return node.end
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+
+        for c in prefix:
+            if c not in node.children:
+                return False
+
+            node = node.children[c]
+
+        return True
+        
